f245fc2dc52faccec14ac27c772d0ddcbc57c1e0
rifatuddin-01 <rifat.uddin01@northsouth.edu>
2020-09-09 02:15:05 +0600

    new file

---
+++ week 7/Lab 10 (string)/cls problems/4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void reverse(char arr[]);
+
+int main()
+{
+   char str[100];
+
+   printf("Enter the string: ");
+   gets(str);
+
+   reverse(str);
+
+   return 0;
+}
+
+ void reverse(char arr[])
+ {
+    int i=0 , j=0 , temp;
+
+   j = strlen(arr) - 1;
+
+   while (i < j)
+    {
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      i++;
+      j--;
+    }
+
+    printf("\nReverse string is: %s\n", arr);
+
+ }
